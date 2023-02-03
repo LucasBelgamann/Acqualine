2581d2000b0ae2c86d1fd9985de73ea16c8cefaa
LucasBelgamann <lucasbelgamann@hotmail.com>
2023-02-03 17:19:12 -0300

    fix: delete provider

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import './App.css';
-import ProviderHeater from './context/Provider';
 import Home from './pages/Home';
 
 function App() {
   return (
-    <ProviderHeater>
       <Home />
-    </ProviderHeater>
   );
 }
 
