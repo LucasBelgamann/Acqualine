e0476e52e057f7250c5c787772075892004baf90
LucasBelgamann <lucasbelgamann@hotmail.com>
2023-02-03 16:48:50 -0300

    fix: products

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -1,17 +1,12 @@
-import img from './img/aquecedor.png';
-import { ProductsData } from './ProductsData';
 import './Products.css';
-import { useContext } from 'react';
-import Context from '../../context/Context';
+import lz1600 from './img/LorenzettiLZ1600DE.png';
+import lz2000 from './img/LZ2000DELorenzetti.png';
+import E15 from './img/E15.png';
+import E21 from './img/E21.png';
+import rheemDigital15 from './img/RheemDigital.png';
+import rheemDigital20 from './img/RheemDigital20Litros.png';
 
 const Products = () => {
-  const { setProduct } = useContext(Context);
-
-
-  const handleProductClick = (value) => {
-    setProduct(value)
-  }
-
   return (
     <div className="container-products">
          <div className="titles-products">
@@ -20,18 +15,54 @@ const Products = () => {
          </div>
         <div>
             <div className="products">
-               {
-                ProductsData.map((product) => (
-                    <div className='card-product'>
-                        <img src={img} alt={product.title} />
-                        <h3>{product.title}</h3>
-                        <h4>{product.price}</h4>
+               <div className='card-product'>
+                        <img src={lz1600} alt="aquecedor1" />
+                        <h4>Aquecedor a gás GN Lorenzetti LZ 1600DE branco 127V/220V</h4>
+                        <h4 className='price'>R$1.900,00</h4>
                        <a href="#contact">
-                       <button onClick={ () => handleProductClick(product) }>Fazer orçamento</button>
+                       <button>Fazer orçamento</button>
                        </a>
                     </div>
-                ))
-               }
+                    <div className='card-product'>
+                        <img src={lz2000} alt="aquecedor2" />
+                        <h4>Aquecedor de Água a Gás LZ2000DE Lorenzetti</h4>
+                        <h4 className='price'>R$ 2.450,00</h4>
+                       <a href="#contact">
+                       <button>Fazer orçamento</button>
+                       </a>
+                    </div>
+                    <div className='card-product'>
+                        <img src={E15} alt="aquecedor3" />
+                        <h4>Aquecedor a Gás E15 - Branco - Bivolt</h4>
+                        <h4 className='price'>R$ 1.950,00</h4>
+                       <a href="#contact">
+                       <button>Fazer orçamento</button>
+                       </a>
+                    </div>
+                    <div className='card-product'>
+                        <img src={E21} alt="aquecedor4" />
+                        <h4>Rinnai - Aquecedor a Gás E21 - Bivolt</h4>
+                        <h4 className='price'>R$ 3.000,00</h4>
+                       <a href="#contact">
+                       <button>Fazer orçamento</button>
+                       </a>
+                    </div>
+                    <div className='card-product'>
+                        <img src={rheemDigital15} alt="aquecedor5" />
+                        <h4>Aquecedor Rheem Digital - Vazão 15 Litros - Branco - GLP</h4>
+                        <h4 className='price'>R$ 2.150,00</h4>
+                       <a href="#contact">
+                       <button>Fazer orçamento</button>
+                       </a>
+                    </div>
+                    <div className='card-product'>
+                        <img src={rheemDigital20} alt="aquecedor6" />
+                        <h4>Aquecedor Rheem Digital - Vazão 20 Litros - Branco</h4>
+                        <h4 className='price'>R$ 3.000,00</h4>
+                       <a href="#contact">
+                       <button>Fazer orçamento</button>
+                       </a>
+                    </div>
             </div>
         </div>
     </div>
--- src/components/products/ProductsData.js
+++ src/components/products/ProductsData.js
@@ -1,32 +1,34 @@
+import E15 from './img/E15.png';
+
 export const ProductsData = [
     {
-      title: 'Aquecedor1',
+      title: 'Aquecedor a gás GN Lorenzetti LZ 1600DE branco 127V/220V',
       ulr: 'https://magazord-public.s3.sa-east-1.amazonaws.com/eletrodecora/img/2022/03/produto/182/3700.png',
-      price: 'R$ 1.000,00'
+      price: 'R$1.900,00'
     },
     {
-      title: 'Aquecedor2',
+      title: 'Aquecedor de Água a Gás LZ2000DE Lorenzetti',
       ulr: 'https://magazord-public.s3.sa-east-1.amazonaws.com/eletrodecora/img/2022/03/produto/182/3700.png',
-      price: 'R$ 2.000,00'
+      price: 'R$ 2.450,00'
     },
     {
-      title: 'Aquecedor3',
+      title: 'Aquecedor a Gás E15 - Branco - Bivolt',
+      ulr: E15,
+      price: 'R$ 1.950,00'
+    },
+    {
+      title: 'Rinnai - Aquecedor a Gás E21 - Bivolt',
       ulr: 'https://magazord-public.s3.sa-east-1.amazonaws.com/eletrodecora/img/2022/03/produto/182/3700.png',
       price: 'R$ 3.000,00'
     },
     {
-      title: 'Aquecedor4',
+      title: 'Aquecedor Rheem Digital - Vazão 15 Litros - Branco - GLP',
       ulr: 'https://magazord-public.s3.sa-east-1.amazonaws.com/eletrodecora/img/2022/03/produto/182/3700.png',
-      price: 'R$ 4.000,00'
+      price: 'R$ 2.150,00'
     },
     {
-      title: 'Aquecedor5',
+      title: 'Aquecedor Rheem Digital - Vazão 20 Litros - Branco',
       ulr: 'https://magazord-public.s3.sa-east-1.amazonaws.com/eletrodecora/img/2022/03/produto/182/3700.png',
-      price: 'R$ 5.000,00'
-    },
-    {
-      title: 'Aquecedor6',
-      ulr: 'https://magazord-public.s3.sa-east-1.amazonaws.com/eletrodecora/img/2022/03/produto/182/3700.png',
-      price: 'R$ 6.000,00'
+      price: 'R$ 3.000,00'
     }
   ];
